defa91369b3b46db653bc40f07cd8135f7df05ff
vovan42002 <91207798+vovan42002@users.noreply.github.com>
2023-03-20 22:53:54 +0200

    Update Jenkinsfile-jmeter.groovy

--- lab3/Jenkinsfile-jmeter.groovy
+++ lab3/Jenkinsfile-jmeter.groovy
@@ -8,25 +8,11 @@ pipeline {
           }
         }
         steps {
-          sh 'jmeter -n -t JMeter-Sample/JMeter-load-test.jmx -JThreadNumber=10 -JRampUpPeriod=1 -Jiterations=10 -l results.csv -e -o /result_output'
+          sh 'cd bin'
+          sh 'git clone https://github.com/praveenkrjha93/JMeter-Sample.git'
+          sh 'sh jmeter -n -t JMeter-Sample/JMeter-load-test.jmx -JThreadNumber=10 -JRampUpPeriod=1 -Jiterations=10 -l results.csv -e -o /result_output'
         }
       }
-        stage('Install dependencies') {
-            agent {
-              docker {
-                image 'maven:latest'
-              }
-            }
-            steps {
-                sh 'cd lab3 && mvn install:install-file -Dfile=libs/Simulation2022.jar -DgroupId=com.simulation -DartifactId=simulation -Dversion=1.0.0 -Dpackaging=jar'
-                sh 'cd lab3 && mvn clean compile assembly:single'
-            }
-        }
-        stage('Check existing runnable jar') {
-            steps {
-                sh 'ls lab3/target'
-            }
-        }
     }
     post {
         always {

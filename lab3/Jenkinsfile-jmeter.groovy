4960a4ebce17fd9d99cafbcd69b14136f7ca7ead
vovan42002 <91207798+vovan42002@users.noreply.github.com>
2023-03-20 23:25:07 +0200

    Update Jenkinsfile-jmeter.groovy

--- lab3/Jenkinsfile-jmeter.groovy
+++ lab3/Jenkinsfile-jmeter.groovy
@@ -4,6 +4,7 @@ pipeline {
       stage('jmeter test') {
         steps {
             sh 'docker run --name jmeter_base vovan4/jmeter_base:latest'
+            sleep 5
             sh "docker exec jmeter_base sh 'cd bin && git clone https://github.com/praveenkrjha93/JMeter-Sample.git'"
             sh 'sh jmeter -n -t JMeter-Sample/JMeter-load-test.jmx -JThreadNumber=10 -JRampUpPeriod=1 -Jiterations=10 -l results.csv -e -o /result_output'
         }

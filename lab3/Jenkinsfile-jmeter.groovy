0bbf24294d40da5193ed4a7f1e665a333e429863
vovan42002 <91207798+vovan42002@users.noreply.github.com>
2023-03-20 23:19:38 +0200

    Update Jenkinsfile-jmeter.groovy

--- lab3/Jenkinsfile-jmeter.groovy
+++ lab3/Jenkinsfile-jmeter.groovy
@@ -8,8 +8,9 @@ pipeline {
             }
         }
         steps {
-          sh 'cd /bin'
-          sh 'ls -la /bin'
+          sh 'ls -la'
+          sh 'cd bin'
+          sh 'ls -la'
           sh 'git clone https://github.com/praveenkrjha93/JMeter-Sample.git'
           sh 'sh jmeter -n -t JMeter-Sample/JMeter-load-test.jmx -JThreadNumber=10 -JRampUpPeriod=1 -Jiterations=10 -l results.csv -e -o /result_output'
         }
